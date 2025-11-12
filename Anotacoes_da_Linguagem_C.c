5174135c59328ff73a9907c4f854c0aa763d05d8
Julia <77999938+yumekojulia@users.noreply.github.com>
2025-11-12 10:32:34 -0300

    Update

--- Anotacoes_da_Linguagem_C.c
+++ Anotacoes_da_Linguagem_C.c
@@ -1257,3 +1257,318 @@ int main() { // FUNÇÃO PRINCIPAL
 
     return 0; // GG! deu tudo certo >.<'
 }
+
+
+CÓDIGO BASE DE MATRIZ
+
+#include <stdio.h>
+#include <locale.h>
+
+#define qtdLin 3
+#define qtdCol 4
+
+void inicializaMatriz(int num[][qtdCol], int qtdLinhas, int qtdColunas)
+{
+    for (int l = 0; l < qtdLinhas; l++)
+        for (int c = 0; c < qtdColunas; c++)
+            num[l][c] = 0;
+}
+
+void leMatriz(int num[][qtdCol], int qtdLinhas, int qtdColunas)
+{
+    for (int l = 0; l < qtdLinhas; l++)
+    {
+        for (int c = 0; c < qtdColunas; c++)
+        {
+            printf("Informe o elemento [%d][%d]: ",l+1,c+1);
+            scanf("%d", &num[l][c]);
+        }
+        printf("\n");
+    }
+    return;
+}
+
+void escreveMatriz(int num[][qtdCol], int qtdLinhas, int qtdColunas)
+{
+    for (int l = 0; l < qtdLinhas; l++)
+    {
+        printf("| ");
+        for (int c = 0; c < qtdColunas; c++)
+            printf("%2d ",num[l][c]);
+        printf("|\n");
+    }
+    return;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese-brazilian");
+    
+    int matriz[qtdLin][qtdCol], colInicial, linInicial;
+    
+    inicializaMatriz(matriz,qtdLin,qtdCol);
+    leMatriz(matriz,qtdLin,qtdCol);
+    escreveMatriz(matriz,qtdLin,qtdCol);
+    
+    for (int l = 0; l < qtdLin; l++) {
+        for (int c = 0; c < qtdCol; c++) {
+            if (c == qtdCol - 1) {
+                colInicial = 0;
+                linInicial = 1 + l;
+            }
+            else {
+                colInicial = c + 1;
+                linInicial = l;
+            }
+            for (int i = linInicial; i < qtdLin; i++) {
+                for (int j = (i == linInicial?colInicial:0); j < qtdCol; j++) {
+                    if (matriz[l][c] == matriz[i][j])
+                        printf("O elemento %d se repete.\n", matriz[l][c]);
+                }
+            }
+        }
+    }
+}
+
+
+
+Enumerações em C
+
+enum eSexo {
+	masculino, // 0
+	feminino // 1
+} sexo;
+
+sexo = masculino;
+sexo = (eSexo)1; // atrib. feminino
+
+printf("%d", sexo)
+
+///////////////////////
+
+enum eSexo {
+	masculino = 2;
+	feminino = 3
+} sexo;
+
+sexo = masculino;
+sexo = (eSexo)3;
+
+printf("%d", sexo);
+
+
+sexo = (eSexo)9; // aceita
+
+///////////////////////////
+
+enum eDiasSemana {
+	domingo = 1,
+	segunda = 2,
+	terca = 3,
+	quarta = 4,
+	quinta = 5,
+	sexta = 6,
+	sabado = 7
+};
+
+enum eDiasSemana hoje; ou eDiasSemana hoje;
+hoje = terca;
+printf("%d", hoje);
+saida ~ 3 
+
+/////////////////////////
+
+enum eFonte { // usar sempre multiplos de 2 nas opções para não dar erro nas combinações
+	ITALICO = 1,
+	NEGRITO = 2,
+	SUBLINHADO = 4
+};
+
+eFonte titulo;
+titulo = NEGRITO;
+titulo = NEGRITO | SUBLINHADO; // bitwise OR | OPERATOR FLAG
+printf("%d", titulo);
+saida ~ 6 // combinação de negrito (2) + sublinhado (4)
+
+
+6 & 1 - falso // 2 + 4 & 1
+
+
+
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "portuguese-brazilian");
+
+    enum eFonte {
+        ITALICO = 1,
+        NEGRITO = 2,
+        SUBLINHADO = 4
+        };
+
+    //enum eFonte titulo;
+    //titulo = (enum eFonte)(NEGRITO | SUBLINHADO);
+    enum eFonte titulo = (enum eFonte)(NEGRITO | SUBLINHADO); // escrita resumida
+
+    if (titulo & ITALICO) // bitwise AND & operator
+        printf("O titulo está em itálico\n");
+    if (titulo & NEGRITO) // bitwise AND & operator
+        printf("O título está em negrito\n");
+    if (titulo & SUBLINHADO) // bitwise AND & operator
+        printf("O título está sublinhado\n");
+}
+
+
+Estruturas em C
+
+. (Ponto)	A Estrutura (a ficha real)			estrutura.campo		Acessa campos em estruturas diretas.
+-> (Seta)	O Ponteiro (o endereço da ficha)	(*ponteiro).campo	Acessa campos através de um ponteiro.
+
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+int main() {
+    setlocale(LC_ALL, "portuguese-brazilian");
+
+    struct Pessoa { // a estrutura possui 3 atributos
+        char nome[50];
+        int matricula;
+        float salario;
+    } dono,funcionarios[20];
+
+    struct Pessoa p1,p2,alunos[100];
+
+    // Acesso aos membros da estrutura:
+    strcpy(p1.nome,"Margela Gomes");
+    p1.matricula = 202039;
+    p1.salario = 2000.00;
+
+    strcpy(alunos[0].nome,"Pedro Henrique Dias");
+    alunos[0].matricula = 202049;
+    alunos[0].salario = 1500.00;
+
+    strcpy(alunos[1].nome,"Heloísa Gastão");
+    alunos[1].matricula = 202051;     // vetor[].atribuição
+    alunos[1].salario = 3509.27;
+
+    struct Pessoa* lider;   // Ponteiros para estruturas e acesso aos seus membros
+    lider = &alunos[1];
+    
+    strcpy(lider -> nome,"Margela Gomes");
+    lider -> matricula = 202039;  // ponteiro -> atribuição
+    lider -> salario = 2000.00;
+    (*lider).salario *= 1.2; // lider -> salario *= 1.2;
+}
+
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+struct pessoa {
+    char nome[50]; // 50 bits
+    int idade; // 4 bits
+    float peso; // 8 bits
+};
+
+int main() {
+    setlocale(LC_ALL, "portuguese-brazilian");
+
+    struct pessoa *chefePtr, chefe;
+    chefePtr = &chefe;
+    printf("Informe a idade: ");
+    scanf("%d", &chefePtr -> idade);
+    printf("Informe o peso: ");
+    scanf("%f", &chefe.peso);
+
+    printf("Resultado:\n");
+    printf("Idade: %d\n", chefe.idade);
+    printf("Peso: %f\n\n", chefePtr -> peso);
+
+    printf("Tamanho da estrutura: %d\n", sizeof(chefe));
+    printf("Tamanho do ponteiro: %d\n", sizeof(chefePtr));
+    printf("Conteúdo do ponteiro: %d\n", sizeof(*chefePtr));
+
+    return 0;
+}
+
+
+//
+// Created by juh on 11/12/25 on CLion
+//
+
+#include <stdio.h>
+#include <locale.h>
+#include <stdbool.h>
+#include <string.h>
+
+enum eSexo { // serve para limitar, tipo uma "etiqueta"
+    Masculino, // valor 0
+    Feminino, // valor 1
+    Outro // valor 3
+};
+
+struct stAluno { // estrutura, como um "molde"
+    int numMatricula;
+    bool ativo;
+    enum eSexo sexo; // Reserva um espaço para a "etiqueta" (eSexo)
+    char nome[101];
+    float valorMensalidade;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese-brazilian");
+
+        struct stAluno alunosAlgII[5]; // delimita o registro para 5 alunos (indices 0 a 4)
+    // preenchimento dos dados do 1º aluno (índice 0):
+        alunosAlgII[0].numMatricula = 1000; // o '.' acessa um campo quando tem a estrutura diretamente
+        alunosAlgII[0].ativo = true;
+        alunosAlgII[0].sexo = Masculino;
+        strcpy(alunosAlgII[0].nome,"Aluno 1"); // strcpy porque o nome é uma string
+        alunosAlgII[0].valorMensalidade = 250.23;
+    // preenchimento dos dados do 2º aluno (índice 1):
+        alunosAlgII[1].numMatricula = 1001;
+        alunosAlgII[1].ativo = true;
+        alunosAlgII[1].sexo = Outro;
+        strcpy(alunosAlgII[1].nome,"Aluno 2");
+        alunosAlgII[1].valorMensalidade = 198.44;
+    // preenchimento dos dados do 3º aluno (índice 2)
+        alunosAlgII[2].numMatricula = 1002;
+        alunosAlgII[2].ativo = true;
+        alunosAlgII[2].sexo = Feminino;
+        strcpy(alunosAlgII[2].nome,"Aluna 1");
+        alunosAlgII[2].valorMensalidade = 200.00;
+    // preenchimento dos dados do 4º aluno (índice 3)
+        alunosAlgII[3].numMatricula = 1003;
+        alunosAlgII[3].ativo = false;
+        alunosAlgII[3].sexo = Feminino;
+        strcpy(alunosAlgII[3].nome,"Aluna 2");
+        alunosAlgII[3].valorMensalidade = 302.10;
+    // preenchimento dos dados do 5º aluno (índice 4)
+        alunosAlgII[4].numMatricula = 1004;
+        alunosAlgII[4].ativo = true;
+        alunosAlgII[4].sexo = Masculino;
+        strcpy(alunosAlgII[4].nome,"Aluno 3");
+        alunosAlgII[4].valorMensalidade = 270.22;
+
+        printf("Tamanho da estrutura: %lu\n\n", sizeof(struct stAluno)); // Mostra quanto espaço de memória cada ficha ocupa
+
+        for (int i=0; i<5; i++) { // loop para imprimir toda estrutura
+            struct stAluno *aluno = alunosAlgII + i; // Cria um ponteiro (aluno) e aponta ele para a ficha atual (alunosAlgII[i])
+            printf("Dados da %dª estrutura: (Endereço: %d)\n",i + 1, aluno); // Imprime o número da ficha e o endereço da memória correspondente
+            printf("N�mero Matrícula: %d\n", aluno -> numMatricula); // Imprime a Matrícula da ficha que o ponteiro (aluno) está apontando
+            printf("Aluno(a) Ativo..: %s\n", aluno -> ativo ? "Sim" : "Não"); // '->' acessa um campo quando tem um ponteiro para a estrutura (o endereço da ficha) e não a ficha em si
+            printf("Sexo............: %s\n",
+                (aluno -> sexo == Masculino) ? "Masculino" :
+                (aluno -> sexo == Feminino)  ? "Feminino" : "Outro");;
+            printf("Nome do Aluno...: %s\n", aluno -> nome);
+            printf("Valor Mensalid..: %4.2f\n", aluno -> valorMensalidade);
+            printf("\n");
+        } // o loop volta para o início e repete para a próxima ficha
+
+    return 0; // GG! :)
+}
+
+
